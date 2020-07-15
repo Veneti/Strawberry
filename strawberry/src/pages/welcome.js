bbe3086cc6f6d9bf8cd6b0486e46b7fa9445f6f0
Autumn Tan <atan@Autumns-MacBook-Pro.local>
2020-07-15 16:47:12 -0500

    added description for welcome

--- strawberry/src/pages/welcome.js
+++ strawberry/src/pages/welcome.js
@@ -17,6 +17,7 @@ class welcome extends Component {
         <header class="welcome-header">
           <img src={Strawberry} alt="logo" style={{ "padding-top": "40px" }}/>
     			<h1 class="rainbow-text">Welcome to Strawberry</h1>
+          <h3 class="welcome-description">a bite-size intro to insurance</h3>
           <svg width="209" height="82" viewBox="0 0 209 82" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g filter="url(#filter0_d)">
               <path d="M203.536 40.5355C205.488 38.5829 205.488 35.4171 203.536 33.4645L171.716 1.64466C169.763 -0.30796 166.597 -0.30796 164.645 1.64466C162.692 3.59728 162.692 6.76311 164.645 8.71573L192.929 37L164.645 65.2843C162.692 67.2369 162.692 70.4027 164.645 72.3553C166.597 74.308 169.763 74.308 171.716 72.3553L203.536 40.5355ZM4 42L200 42L200 32L4 32L4 42Z" fill="#E11313"/>
