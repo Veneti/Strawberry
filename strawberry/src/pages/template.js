c60347701668603a33e8cf4d7d0725b134550d0a
Autumn Tan <atan@Autumns-MacBook-Pro.local>
2020-07-15 17:25:33 -0500

    polish

--- strawberry/src/pages/template.js
+++ strawberry/src/pages/template.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
+import '../App.css';
 
 const styles = (theme) => ({
 	example: {
@@ -8,16 +9,26 @@ const styles = (theme) => ({
 		textAlign: 'center'
 	}
 });
+
 class template extends Component {
-    render() {
-		const { classes } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      zip: '',
+      ageRange: ''
+    }
+  }
+
+  render() {
 		return (
-			
 			<div>
-				<h1 className={classes.example}>Hello!</h1>
-				<a href="/strawberry">Link to strawberry</a>
-            </div>
+				<h1 className="general-heading">Hello!</h1>
+        <div>
+        </div>
+      </div>
 		);
 	}
 }
+
 export default withStyles(styles)(template);
