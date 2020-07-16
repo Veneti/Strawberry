f606bfbea8588eb5f4a02cea9cac74d7596c63af
Sruthi Rudravajjala <sruthi.rudravajjala.vaauwp@statefarm.com>
2020-07-15 21:29:14 -0500

    centered text + style

--- strawberry/src/App.js
+++ strawberry/src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import strawberry from './pages/strawberry';
 import template from './pages/template';
-
+import explanation from './pages/explanation';
 import React from 'react';
+
+
 function App() {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App() {
         <Switch>
             <Route exact path="/strawberry" component={strawberry}/>
             <Route exact path="/template" component={template}/>
+            <Route exact path="/explanation" component={explanation}/>
         </Switch>
       </div>
     </Router>
