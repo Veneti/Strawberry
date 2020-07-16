d47e3117d5e4db07925570419781325d983316cf
hacker-vaauwt <brian.truong.vaauwt@statefarm.com>
2020-07-15 20:13:44 -0500

    Added scss style and recommend page

--- strawberry/src/App.js
+++ strawberry/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import strawberry from './pages/strawberry';
 import template from './pages/template';
 import welcome from './pages/welcome';
+import recommend from './pages/recommend';
 
 import React from 'react';
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Route exact path="/strawberry" component={strawberry}/>
             <Route exact path="/template" component={template}/>
             <Route exact path="/welcome" component={welcome}/>
+            <Route exact path="/recommend" component={recommend}/>
         </Switch>
       </div>
     </Router>
