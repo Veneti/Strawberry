e9c527a4c3b452c0331a74ea94963b03c395413f
hacker-vaakfy <cady.baltz.vaakfy@statefarm.com>
2020-07-15 20:44:29 -0500

    finished all questions

--- strawberry/src/App.js
+++ strawberry/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import strawberry from './pages/strawberry';
 import template from './pages/template';
+import questions from './pages/questions';
 
 import React from 'react';
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Switch>
             <Route exact path="/strawberry" component={strawberry}/>
             <Route exact path="/template" component={template}/>
+            <Route exact path="/questions" component={questions}/>
         </Switch>
       </div>
     </Router>
--- strawberry/src/components/jake.js
+++ strawberry/src/components/jake.js
@@ -1,53 +1,29 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import jake from '../images/jake.png'
+import jakeImage from '../images/jake.png'
 
 const styles = (theme) => ({
-	example: {
-		color: 'red',
-		//text-align -> textAlign
-		textAlign: 'center'
-	},
-	jakediv: {
-		height: '100%'
-	},
-	jake: {
-		align: 'right',
-		verticalAlign: 'bottom',
-		width: '20%'
-	}
+
 });
 var style = {
     textAlign: "right",
     padding: "20px",
     position: "fixed",
-    left: "0",
+    right: "0",
     bottom: "0",
-    height: "200px",
-    width: "90%",
+    height: "250px",
+    width: "20%",
 }
 
-var phantom = {
-  display: 'block',
-  padding: '20px',
-  height: '200px',
-  width: '90%',
-}
 class jake extends Component {
     render() {
 		const { classes } = this.props;
 		return (
 			
 			<div>
-				<h1 className={classes.example}>Hello!</h1>
-				<a href="/strawberry">Link to strawberry</a>
-				<div class="-oneX-row -oneX-col-6">
-					<button type="submit" class="-oneX-btn-primary">Submit</button>
-				</div>
 				<div>
-					<div style={phantom} />
 					<div style={style}>
-						<img src={jake} className={classes.jake}/>
+						<img src={jakeImage} className={classes.jakeStyle}/>
 					</div>
 				</div>
             </div>
